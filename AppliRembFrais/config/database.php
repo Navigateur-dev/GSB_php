cd3570db7eb64de5797f3cc03fd9ba84812c80e4
Nicolas Congiu <44933198+Navigateur-dev@users.noreply.github.com>
2022-03-28 20:27:24 +0200

    Update database.php

--- AppliRembFrais/config/database.php
+++ AppliRembFrais/config/database.php
@@ -1,17 +1,17 @@
 <?php
 $configDatabaseDev = [
-    'dbname'  => 'rembours_frais',
-    'user'    => 'appli_rbt_frais',
-    'pwd'     => 'JEub@e2021PgLf',
+    'dbname'  => 'xxx',
+    'user'    => 'xxx',
+    'pwd'     => 'xxx',
     'host'    => 'localhost',
     'port'    => '3307',
     'charset' => 'UTF8',
 ];
 // base mysql : port 3307
 //$configDatabase = [
-//    'dbname'  => 'stsjr1615583_2d6rmi',
-//    'user'    => 'stsjr1615583',
-//    'pwd'     => '7r7pq3fn5y',
+//    'dbname'  => 'xxx',
+//    'user'    => 'xxxx',
+//    'pwd'     => 'xxx',
 //    'host'    => '91.216.107.183',
 //    'port'    => '3307',
 //    'charset' => 'UTF8',
